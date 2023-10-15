8cae16a2f45fcf4e011314110d3e9336ab38d6bf
Deleon09 <alberto101481@gmail.com>
2023-10-14 20:47:01 -0600

    :tada: Begin ManejadorPresupuestos

---
+++ ManejadorPresupuesto/ManejadorPresupuesto/Controllers/TiposCuentasController.cs
@@ -0,0 +1,26 @@
+﻿using ManejadorPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejadorPresupuesto.Controllers
+{
+    public class TiposCuentasController: Controller
+    {
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+
+            return View();
+        }
+
+    }
+}
